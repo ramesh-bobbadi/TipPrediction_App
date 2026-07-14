36352033ece2d5fdc26f113ca03e8a17fde35c8a
ramesh-bobbadi <bobbadiramesh6@gmail.com>
2026-07-14 15:31:38 +0530

    api change

--- backend/tip_prediction/tip_prediction/settings.py
+++ backend/tip_prediction/tip_prediction/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
